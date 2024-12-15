59b40bbb4039d51e0dd7b78d49fddce41f7a4f6f
PandaHood <niksanderson0@gmail.com>
2024-12-15 13:48:13 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import os
 import RPi.GPIO as GPIO
+import time
 
 
 GPIO.setmode(GPIO.BCM)
@@ -7,4 +8,12 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 GPIO.setup(4,GPIO.OUT)
+
+try:
+    while True:
+        time.sleep(0.1)
+except:
+    GPIO.cleanup()
+
+
 
