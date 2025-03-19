14b50877b2ca3a65f20f41c9b0a9c15361e4af58
PandaHood <niksanderson0@gmail.com>
2025-03-19 15:02:00 -0400

    fghj

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import src.util as util
 ## Main loop
 
 def main():
-    team_number= input("Team Number: ")
+    team_number = input("Team Number: ")
     # check what else we need to print out
     print(f"Team_number: {team_number}, Team_name: NUWave, Time: {datetime.datetime.now()}")
 
@@ -21,7 +21,6 @@ def main():
     time.sleep(1)
     util.output_txt(time.time() - start,5,5)
 
-    # define main loop 
 
 
 if __name__ == "__main__":
