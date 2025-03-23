0b93f22fce6db4987290c01e69cfbc2eb59c72ea
PandaHood <niksanderson0@gmail.com>
2025-03-23 17:44:00 -0400

    Update test_depth_sensor.py

--- testing/depth_sensor/test_depth_sensor.py
+++ testing/depth_sensor/test_depth_sensor.py
@@ -1,3 +1,6 @@
+import sys, os
+# Append the parent directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
 from depth_sensor import *
 
 if __name__ == "__main__":
