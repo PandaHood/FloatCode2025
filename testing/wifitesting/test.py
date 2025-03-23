6d55dbc187a54b1ab8d193c73bf3add78a75ef8e
PandaHood <niksanderson0@gmail.com>
2025-03-23 17:06:38 -0400

    altered led test

--- testing/wifitesting/test.py
+++ testing/wifitesting/test.py
@@ -1,10 +1,13 @@
 from gpiozero import LED
-from time import sleep
+from time import *
 
 red = LED(4)
-
+start = time()
 while True:
+
     red.on()
     sleep(1)
     red.off()
     sleep(1)
+    if time() - start >= 60:
+        break
