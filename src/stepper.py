0fbc720cff05fcd317284e69a22537df3bc99be2
PandaHood <niksanderson0@gmail.com>
2025-06-05 19:29:12 -0400

    Stepper motor

--- src/stepper.py
+++ src/stepper.py
@@ -50,20 +50,9 @@ class TicI2C(object):
     if position >= (1 << 31):
       position -= (1 << 32)
     return position
- 
-# Open a handle to "/dev/i2c-3", representing the I2C bus.
-bus = SMBus(1)
- 
-# Select the I2C address of the Tic (the device number).
-address = 14
- 
-tic = TicI2C(bus, address)
+  
+  def move_up():
+    pass
 
-position = tic.get_current_position()
-print("Current position is {}.".format(position))
- 
-new_target = -200 if position > 0 else 200
-new_target = int(input("Position: "))
-print("Setting target position to {}.".format(new_target))
-tic.exit_safe_start()
-tic.set_target_position(new_target)
+  def move_down():
+    pass
