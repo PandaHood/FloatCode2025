a36c00879072d808a6fb9dd73808d24460e7b1f2
PandaHood <niksanderson0@gmail.com>
2025-06-04 21:53:21 -0400

    Update stepper.py

--- stepper.py
+++ stepper.py
@@ -58,11 +58,12 @@ bus = SMBus(1)
 address = 14
  
 tic = TicI2C(bus, address)
- 
+
 position = tic.get_current_position()
 print("Current position is {}.".format(position))
  
 new_target = -200 if position > 0 else 200
+new_target = int(input("Position: "))
 print("Setting target position to {}.".format(new_target))
 tic.exit_safe_start()
 tic.set_target_position(new_target)
